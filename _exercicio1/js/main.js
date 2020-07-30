0305c03a82dd6f3829f82a0f147bf077c2c82d13
Jeferson Silva <jotajeff@gmail.com>
2020-07-30 13:06:35 -0300

    exercício de Reac e Node - noções básicas

---
+++ _exercicio1/js/main.js
@@ -0,0 +1,46 @@
+/* component  */
+
+var Nav = React.createClass({
+    render:function (){
+        return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+	 <div className="navbar-header">
+	 
+	 <a href={this.props.linkURL} className="navbar-brand">
+	 {this.props.title}
+	 </a>
+	
+	</div>
+	
+	</nav>
+        );
+    }
+});
+
+
+/*  component Title  */
+var Title = React.createClass({
+
+    render:function (){
+    return(
+        <div className="container">
+            <div className="row">
+                <h1>{ this.props.title }</h1>
+            </div>
+        </div>
+
+    );
+    }
+    });
+
+    /* Rendering  */
+
+    ReactDOM.render(
+        <Nav title="React" linkURL="index.html" />,
+        document.getElementById('nav')
+    );    
+
+ReactDOM.render(
+    <Title title="Welcome to React" />,
+    document.getElementById('title')
+);
